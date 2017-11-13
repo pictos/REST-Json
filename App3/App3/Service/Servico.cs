4d15f1cda1ea44a0ac7105b21b31b81ab3c63bc1
Pedro Jesus <pedrojesus.cefet@gmail.com>
2017-11-13 13:59:46 -0200

    Adicionar arquivos de projeto.

---
+++ App3/App3/Service/Servico.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Net;
+using App3.Model;
+
+namespace App3.Service
+{
+   public class Servico
+   {
+        private HttpClient _client = new HttpClient();
+        private List<UsuarioModel> _usuarios;
+
+        public async Task<List<UsuarioModel>> PegarUsuariosAsync()
+        {
+            try
+            {
+                string url = string.Format("https://jsonplaceholder.typicode.com/users/");
+                var resposta = await _client.GetAsync(url);
+                if (resposta.StatusCode == HttpStatusCode.NotFound)
+                {
+                    _usuarios = new List<UsuarioModel>();
+                }
+                else
+                {
+                    var content = await resposta.Content.ReadAsStringAsync();
+                    var json = JsonConvert.DeserializeObject<List<UsuarioModel>>(content);
+                    _usuarios = new List<UsuarioModel>(json);
+                }
+                return _usuarios;
+                
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+   }
+}
---
+++ App3/App3/ViewModel/UsuariosViewModel.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using App3.Model;
+using App3.Service;
+
+namespace App3.ViewModel
+{
+   public class UsuariosViewModel : BaseViewModel
+   {
+        private bool _isBusy;
+
+        public bool IsBusy
+        {
+            get { return _isBusy; }
+            set { _isBusy = value; OnPropertyChanged();
+                CarregarCommand.ChangeCanExecute(); }
+        }
+
+        private Servico _servico;
+
+        public Command CarregarCommand { get; }
+
+        private List<UsuarioModel> _user;
+
+        public List<UsuarioModel> User
+        {
+            get { return _user; }
+            set { _user = value; OnPropertyChanged(); }
+        }
+
+        public UsuariosViewModel()
+        {
+            CarregarCommand = new Command(
+                async () => await ExecuteCarregarCommand(), () => !IsBusy);
+            User = new List<UsuarioModel>();
+            _servico = new Servico();
+        }
+
+        async Task ExecuteCarregarCommand()
+        {
+            if (!IsBusy)
+            {
+                Exception Error = null;
+                try
+                {
+                    IsBusy = true;
+                    User = await _servico.PegarUsuariosAsync();
+                }
+                catch (Exception ex)
+                {
+
+                    Error = ex;
+                    await DisplayAlert("Algo deu errado",
+                        $"Ocorreu o erro:{Error.Message}.", "Ok");
+                }
+                finally
+                {
+                    IsBusy = false;
+                }
+            }
+        }
+    }
+}
